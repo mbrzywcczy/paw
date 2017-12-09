5dc230cfacc4ea6e624a181469906c587363bd74
mrcngrcki <marccin95@gmail.com>
2017-12-09 16:57:00 +0100

    logowanie i rejestracja

---
+++ weselaPAW/login.php
@@ -0,0 +1,28 @@
+<?php
+require ('./functions.php');
+	session_start();
+
+if(isset($_SESSION['login']))
+{
+	header('Location:log_ok.php');
+	exit;
+}
+else
+{
+	if(isset($_POST['log_button']))
+	{
+	$login = trim($_POST['log_login']);
+	$password = trim($_POST['log_password']);
+	$pass_hash = sha1($password);
+	
+	
+	$object = new functions('PDO');
+	$object -> login($login,$pass_hash);
+	}
+}
+?>
+<form method="POST" action="login.php">
+Login: <input name="log_login"></br>
+Haslo: <input type=password name="log_password"></br>
+<input type="submit" name="log_button" value="Zaloguj">
+</form>
---
+++ weselaPAW/register.php
@@ -0,0 +1,48 @@
+﻿<?php
+require ('./functions.php');
+
+if (isset($_POST['frm_register']))
+{
+	//czyści formularz
+	$login = trim($_POST['frm_login']);
+	$password1 = trim($_POST['frm_pass1']);
+	$password2 = trim($_POST['frm_pass2']);
+	
+	//zmienna z błędami
+	$errors = NULL;
+	
+	//sprawdza czy dane są poprawne
+	if(strlen($login)<4)
+	{
+		$errors .= 'Login musi miec co najmniej 4 znaki</br>';
+	}
+	if(strlen($password1)<6)
+	{
+		$errors .= 'Haslo musi miec co najmniej 6 znakow</br>';
+	}
+	if($password1 !== $password2)
+	{
+		$errors .= 'Podane hasla nie sa takie same</br>';
+	}
+	
+	//sprawdza czy użytkownik o danym loginie już nie istnieje
+	$object = new functions('PDO');
+	$object -> check_reg();
+	
+	if(empty($errors))
+	{
+		$object -> register();
+	}
+	else
+	{
+		echo $errors;
+	}
+}
+
+?>
+<form method="POST" action="register.php">
+Login: <input name="frm_login"></br>
+Haslo: <input type="password" name="frm_pass1"></br>
+Powtorz haslo: <input type="password" name="frm_pass2"></br>
+<input type="submit" name="frm_register" value="Utworz konto">
+</form>
